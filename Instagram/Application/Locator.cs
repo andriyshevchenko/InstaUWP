fea4d4a1bb82b834f3f405ebf9f9ba9d95d7428f
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-06-28 18:50:59 +0300

    supposed to work version

---
+++ Instagram/Application/Locator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instagram.Application
+{
+    public class Locator 
+    {
+        static Lazy<MainViewModel> _mainViewModel = new Lazy<MainViewModel>(true);
+        public static MainViewModel MainViewModel => _mainViewModel.Value;
+    }
+}
