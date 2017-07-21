c8a93ed0e004166b4e97601e328eb11be69be9f9
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-07-21 20:04:59 +0300

    tests running on a new build .10586

--- test/GalaSoft.MvvmLight.Extensions.Test/Xaml/ViewModelAccessorTest.cs
+++ test/GalaSoft.MvvmLight.Extensions.Test/Xaml/ViewModelAccessorTest.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GalaSoft.MvvmLight.Extensions.Xaml;
+using InputValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static System.Collections.Generic.Create;
 
 namespace GalaSoft.MvvmLight.Extensions.Xaml.Tests
 {
@@ -8,7 +12,10 @@ namespace GalaSoft.MvvmLight.Extensions.Xaml.Tests
         [TestMethod]
         public void should_get_view_model_for_required_child_name()
         {
-
+            object test = new object();
+            Assert.IsTrue(
+                new ViewModelAccessor()
+                .Convert(dictionary(("main", test)), null, "main", null) == test);
         }
     }
 }
